ba47a3d6b5b9af16cc26f3f1c73a5247faff98d5
Alexander <mypost@example.com>
2023-01-29 10:58:36 +0300

    1

---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿// task 60. Сформировать трехмерный массив из неповторяющихся двузначных чисел.
+// Вывести значения с указанием индексов каждого элемента.
+
+Console.Clear();
+
+void NewMatrix(int[,,] array3D)
+{
+  int[] temp = new int[array3D.GetLength(0) * array3D.GetLength(1) * array3D.GetLength(2)];
+  int  num = 0;
+  for (int i = 0; i < temp.GetLength(0); i++)
+  {
+    temp[i] = new Random().Next(10, 100);
+    num = temp[i];
+    if (i >= 1)
+    {
+      for (int j = 0; j < i; j++)
+      {
+        while (temp[i] == temp[j])
+        {
+          temp[i] = new Random().Next(10, 100);
+          j = 0;
+          num = temp[i];
+        }
+          num = temp[i];
+      }
+    }
+  }
+  int count = 0; 
+  for (int x = 0; x < array3D.GetLength(0); x++)
+  {
+    for (int y = 0; y < array3D.GetLength(1); y++)
+    {
+      for (int z = 0; z < array3D.GetLength(2); z++)
+      {
+        array3D[x, y, z] = temp[count];
+        count++;
+      }
+    }
+  }
+}
+
+void ShowArray (int[,,] array3D)
+{
+  for (int i = 0; i < array3D.GetLength(0); i++)
+  {
+    for (int j = 0; j < array3D.GetLength(1); j++)
+    {
+      for (int k = 0; k < array3D.GetLength(2); k++)
+      {
+        Console.WriteLine($"{array3D[i,j,k]} ({i},{j},{k})");
+      }
+    }
+  }
+}
+
+int[,,] Matrix3D = new int[2,2,2];
+NewMatrix(Matrix3D);
+ShowArray(Matrix3D);
